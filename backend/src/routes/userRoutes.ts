05e8e30e20f5bd42ad08e9a9d79164c71f287e1e
Aksh4code <akkisingh8266@gmail.com>
2024-11-16 02:44:36 +0530

    secured routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,3 +1,4 @@
+import { signinInput,signupInput } from "@aksh4code/blog-app-common";
 import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
@@ -16,6 +17,13 @@ userRouter.post('/signup', async (c) => {
     }).$extends(withAccelerate())
   
     const body = await c.req.json()
+    const {success} = signupInput.safeParse(body)
+    if (!success) {
+      c.status(411)
+      return c.json({
+        msg : "Inputs are incorret"
+      })
+    }
     
     try {
         const user = await prisma.user.create({
@@ -39,6 +47,13 @@ userRouter.post('/signin', async (c) => {
       }).$extends(withAccelerate())
       
     const body = await c.req.json()
+    const {success} = signinInput.safeParse(body)
+    if (!success) {
+      c.status(411)
+      return c.json({
+        msg : "Inputs are incorret"
+      })
+    }
       
     const user = await prisma.user.findFirst({
         where : {
