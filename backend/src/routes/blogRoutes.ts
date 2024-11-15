db34d45a673e8eeff3ebaab8c4ec192c96b0ae20
Aksh4code <akkisingh8266@gmail.com>
2024-11-16 00:41:50 +0530

    completed blog routes

--- backend/src/routes/blogRoutes.ts
+++ backend/src/routes/blogRoutes.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
@@ -7,20 +7,49 @@ export const blogRouter = new Hono<{
 	Bindings: {
 		DATABASE_URL: string,
         JWT_SECRET  : string
-	}
+	},
+    Variables : {
+        userId : string
+    }
 }>()
 
-blogRouter.use('*', async (c, next) => {
+blogRouter.use('/*', async (c, next) => {
     const header = c.req.header("Authorization") || "";
     const response =  await verify(header, c.env.JWT_SECRET)
-  
-    if (response.id) {
-      await next()
+    
+    if (response && response.id) {
+        c.set("userId", response.id as string)
+        await next()
     } else {
-      c.status(403)
-      return c.json({msg : "unauthorized"})
+        c.status(403)
+        return c.json({msg : "unauthorized"})
     }
-  
+})
+
+// things to do :: Add pagination 
+
+blogRouter.get('/bulk', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate())
+    console.log("bulk triggered");
+    
+    try {
+        const blogs = await prisma.post.findMany();
+        console.log('Fetched blogs:', blogs)
+
+        if (!blogs || blogs.length === 0) {
+            c.status(404)
+            return c.text("Error while fetching blogs")
+        }
+
+        return c.json ({
+            blogs : blogs
+        })
+    } catch (error) {
+        c.status(500)
+        return c.text("Error while fetching blogs")
+    }
 })
 
 blogRouter.post('/', async (c) => {
@@ -29,12 +58,13 @@ blogRouter.post('/', async (c) => {
     }).$extends(withAccelerate())
 
     const body = await c.req.json()
+    const authorId = c.get("userId")
     try {
         const blog = await prisma.post.create({
             data : {
                 title : body.title,
                 content : body.content,
-                aurhorId: "1"
+                aurhorId: authorId
             }
         })
 
@@ -75,7 +105,9 @@ blogRouter.get('/:id', async (c) => {
 
 })
 
-blogRouter.put('/', async (c) => {
+blogRouter.put('/update', async (c) => {
+    console.log("put triggerd");
+    const userId = c.get('userId');
     const prisma = new PrismaClient({
         datasourceUrl: c.env?.DATABASE_URL,
     }).$extends(withAccelerate())
@@ -84,7 +116,8 @@ blogRouter.put('/', async (c) => {
     try {
         const blog = await prisma.post.update({
             where : {
-                id: body.id
+                id: body.id,
+                aurhorId : userId
             },
             data : {
                 title : body.title,
@@ -93,36 +126,12 @@ blogRouter.put('/', async (c) => {
         })
 
         return c.json({
-            id : blog.id
+            msg : "blog with id " + blog.id + " is updated"
         })
+        
         
     } catch (error) {
         c.status(403)
         return c.json({msg : "some error happend in updating blog"})
     }
-    return c.text(' blogs')
-})
-
-blogRouter.get('/bulk', async (c) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env?.DATABASE_URL,
-    }).$extends(withAccelerate())
-
-    try {
-        const blogs = await prisma.post.findMany()
-        if (!blogs) {
-            c.status(403)
-            return c.text("Error while fetching blogs")
-        }
-
-        return c.json ({
-            blogs
-        })
-    } catch (error) {
-        c.status(403)
-        return c.text("Error while fetching blogs")
-    }
-
-
-
 })
