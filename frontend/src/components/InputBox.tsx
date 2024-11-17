4c961bdc68ce608834b4d3c897a7aa18bba75396
Aksh4code <akkisingh8266@gmail.com>
2024-11-18 01:40:19 +0530

    added type safety

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -1,12 +1,17 @@
+import { ChangeEvent } from "react"
+interface inputBoxType {
+  label : string,
+  placeholder : string,
+  onChange : (e : ChangeEvent<HTMLInputElement>) => void
+}
 
-
-const InputBox = ({label,onChange,placeholder}) => {
+const InputBox = ({label,onChange,placeholder} : inputBoxType) => {
   return (
     <div className="pt-2 ">
         <div className="font-semibold ">
             {label}
         </div>
-        <input className="border px-3 rounded w-full p-1"
+        <input className="border px-3 rounded-lg w-full p-1"
         onChange={onChange} 
         placeholder={placeholder} 
         type="text" 
--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,12 +1,15 @@
+interface quoteType {
+    quote: string; 
+}
 
-const Quote = () => {
+const Quote = ({quote} : quoteType) => {
   return (
     <>
         <div className="bg-gray-300 h-screen flex justify-center flex-col ">
             <div className="flex justify-center">
                 <div className="max-w-md ">
-                    <div className="text-3xl font-bold items-center">
-                    "Quote from my side and it should be above of your head. and what you are doing awiht ksadba adasdjhajs dasdh asd dfgsdfgdsf fdf"
+                    <div className="text-3xl font-bold items-center ">
+                    {quote}
                     </div>
                     <div className="pt-6 max-w-md">
                         - Author saab 
