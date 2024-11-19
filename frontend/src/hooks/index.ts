7ad1a9195310a27c98655a67fa60ff2a2f254870
Aksh4code <akkisingh8266@gmail.com>
2024-11-20 02:56:26 +0530

    created useBlog hook

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { SERVER_URL } from "../../config"
 
-interface Blog {
+interface Blogs {
     content: string,
     title: string,
     id: string,
@@ -11,9 +11,11 @@ interface Blog {
     }
 }
 
+
+
 export const useBlogs = () => {
 
-    const [blogs, setBlogs] = useState<Blog[]>([])
+    const [blogs, setBlogs] = useState<Blogs[]>([])
     const [loading, setLoading] = useState(true)
 
 
@@ -34,6 +36,46 @@ export const useBlogs = () => {
     return {
         loading, 
         blogs
+    }
+
+}
+
+interface Blog {
+    content: string,
+    title: string,
+    id: string,
+    author: {
+        name: string
+    }
+}
+
+export const useBlog = ({id} : {id : string}) => {
+
+    const [blog, setBlog] = useState<Blog>()
+    const [loading, setLoading] = useState(true)
+
+
+    const fetchBlogs = async () => {
+        try {
+            const res = await axios.get(`${SERVER_URL}/api/v1/blog/${id}`, {
+                headers : {
+                    Authorization : localStorage.getItem("token")
+                }})
+                console.log(res.data);
+                setBlog(res.data.blog)
+                setLoading(false)    
+        } catch (error) {   
+            alert("Error while fetching blog")
+        }
+    }
+
+    useEffect(()=>{
+       fetchBlogs()
+    },[id])
+
+    return {
+        loading, 
+        blog
     }
 
 }
