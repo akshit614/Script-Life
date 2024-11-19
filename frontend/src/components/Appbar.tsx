ec6db93a1a43b1405c3d14bae10706fd29e1f3d7
Aksh4code <akkisingh8266@gmail.com>
2024-11-20 02:57:01 +0530

    created appbar component

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,15 +1,28 @@
+import { Link } from "react-router-dom"
 
 const Appbar = () => {
   return (
     <div className="flex justify-between border-b px-5 sm:px-10 items-center p-4">
-      <div className="font-bold text-4xl items-center">
-        BlogApp
-      </div>
-      <div>
-        <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-400 rounded-full mx-2">
+      <Link to={"/blogs"} className="cursor-pointer">
+        <div className="font-bold text-4xl items-center">
+          BlogApp
+        </div>
+      </Link>
+      <div className="flex items-center gap-4">
+        <Link to={"/blogs"} className="cursor-pointer">
+          <div className="flex flex-col justify-center font-bold ">
+            Home
+          </div>
+        </Link>
+        <div> 
+          <Link to={"/blog/publish"}>
+          <button type="button" className=" text-white font-bold bg-green-600 hover:bg-green-500 rounded-full text-sm px-5 py-2.5 text-center">New Blog</button>
+          </Link>
+        </div>
+        <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-400 rounded-full ">
               <span className="font-bold text-2xl text-black ">A</span>
         </div>   
-      </div>
+      </div> 
     </div>
   )
 }
