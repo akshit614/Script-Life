dfbd88aa4482e0e69a9f9bd893afb0b7d1e35d18
Aksh4code <akkisingh8266@gmail.com>
2024-11-27 18:17:20 +0530

    added arrow component

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,13 +3,13 @@ import Auth from "../components/Auth"
 
 const Signin = () => {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-1 md:grid-cols-2">
       <div className="flex justify-center ">
           <div className="w-7/12">
           <Auth type="signin"/>
           </div>
       </div>
-      <div className="invisible md:visible">
+      <div className="hidden md:block">
           <Quote name="Steve Jobs" quote={"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking."} />
       </div>
     </div>
